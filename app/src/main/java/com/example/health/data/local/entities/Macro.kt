2d74eb5d33b0537216c2fc94e0bc34ed134ec1fd
Ngo Ngoc Quyen <quyenjup@gmail.com>
2025-04-09 12:22:22 +0700

    Add Macro entity

---
+++ app/src/main/java/com/example/health/data/local/entities/Macro.kt
@@ -0,0 +1,45 @@
+package com.example.health.data.local.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import com.google.firebase.firestore.PropertyName
+
+@Entity(
+    tableName = "macro",
+    foreignKeys = [ForeignKey(
+        entity = Account::class,
+        parentColumns = ["uid"],
+        childColumns = ["uid"],
+    )]
+)
+data class Macro(
+    @JvmField
+    @PropertyName("uid")
+    @PrimaryKey
+    @ColumnInfo(name = "uid", index = true) val Uid: String,
+
+    @JvmField
+    @PropertyName("calo")
+    @ColumnInfo(name = "calo") var Calo: Float,
+
+    @JvmField
+    @PropertyName("protein")
+    @ColumnInfo(name = "protein") var Protein: Float,
+
+    @JvmField
+    @PropertyName("fat")
+    @ColumnInfo(name = "fat") var Fat: Float,
+
+    @JvmField
+    @PropertyName("carb")
+    @ColumnInfo(name = "carb") var Carb: Float,
+
+    @JvmField
+    @PropertyName("tdee")
+    @ColumnInfo(name = "TDEE") var TDEE: Float,
+
+){
+    constructor() :this("", 0f, 0f, 0f, 0f, 0f)
+}

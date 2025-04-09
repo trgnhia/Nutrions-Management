64e3668803da06452d7e8c7ac68bf3bfd41e08b9
trnghia <trnghia1507@gmail.com>
2025-04-10 00:47:59 +0700

    delete conflict

--- app/src/main/java/com/example/health/data/local/appdatabase/AppDatabase.kt
+++ app/src/main/java/com/example/health/data/local/appdatabase/AppDatabase.kt
@@ -7,19 +7,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.health.data.local.converters.Converters
 import com.example.health.data.local.daos.*
-import com.example.health.data.local.daos.quyen.BurnOutCaloPerDayDao
-import com.example.health.data.local.daos.quyen.CustomFoodDao
-import com.example.health.data.local.daos.quyen.EatenDishDao
-import com.example.health.data.local.daos.quyen.EatenMealDao
-import com.example.health.data.local.daos.quyen.MacroDao
-import com.example.health.data.local.daos.quyen.TotalNutrionsPerDayDao
 import com.example.health.data.local.entities.*
-import com.example.health.data.local.entities.quyen.BurnOutCaloPerDay
-import com.example.health.data.local.entities.quyen.CustomFood
-import com.example.health.data.local.entities.quyen.EatenDish
-import com.example.health.data.local.entities.quyen.EatenMeal
-import com.example.health.data.local.entities.quyen.Macro
-import com.example.health.data.local.entities.quyen.TotalNutrionsPerDay
 
 @Database(
     entities = [
@@ -40,7 +28,7 @@ import com.example.health.data.local.entities.quyen.TotalNutrionsPerDay
         TotalNutrionsPerDay::class,
         Macro::class
     ],
-    version = 11,
+    version = 12,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
